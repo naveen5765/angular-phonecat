716025557505de3657fb7ec6ccc5fd0a74e3a1d9
naveen5765 <anaveen_7691@yahoo.com>
2023-03-09 17:30:33 +0100

    first commit

--- app/app.config.js
+++ app/app.config.js
@@ -1,16 +1,19 @@
-'use strict';
+"use strict";
 
-angular.
-  module('phonecatApp').
-  config(['$routeProvider',
-    function config($routeProvider) {
-      $routeProvider.
-        when('/phones', {
-          template: '<phone-list></phone-list>'
-        }).
-        when('/phones/:phoneId', {
-          template: '<phone-detail></phone-detail>'
-        }).
-        otherwise('/phones');
-    }
-  ]);
+angular.module("phonecatApp").config([
+  "$routeProvider",
+  "$locationProvider",
+  function config($routeProvider, $locationProvider) {
+    $routeProvider
+      .when("/phones", {
+        template: "<phone-list></phone-list>",
+      })
+      .when("/phones/:phoneId", {
+        template: "<phone-detail></phone-detail>",
+      })
+      .otherwise("/phones");
+
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix("!");
+  },
+]);
